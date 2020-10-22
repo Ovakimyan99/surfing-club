fe8aec3bce054f27f340a8261cdd76c7acd35f7c
Ovakimyan99 <50456700+Ovakimyan99@users.noreply.github.com>
2020-10-22 22:42:07 +0300

    Add files via upload
    Подключенные скрипты, дополненная верстка - исправлены некоторые моменты, скорее дополнены

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
-console.log('Hello Surfing!');
+$(document).ready(function() {
+    
+    //sliders:
+
+    // header slider
+
+    const headerSlider = $('#headerSlider');
+    
+    headerSlider.on('changed.owl.carousel', function(e){
+        $('.slide-controls-number__active').text(e.item.index + 1);
+
+        $('.slide-controls-number__total').text(e.item.count);
+    });
+
+    headerSlider.on('initialized.owl.carousel', function(e){
+        $('.slide-controls-number__active').text(e.item.index + 1);
+
+        $('.slide-controls-number__total').text(e.item.count);
+    });
+
+
+    headerSlider.owlCarousel({
+        center: true,
+        items:1,
+        margin: 200,
+        dots: false,
+        smartSpeed: 1300
+    });
+ 
+
+    // Go to the next item
+    $('#headerSliderRight').click(function() {
+        headerSlider.trigger('next.owl.carousel', [1300]);
+    });
+
+    // Go to the previous item
+    $('#headerSliderLeft').click(function() {
+        headerSlider.trigger('prev.owl.carousel', [1300]);
+    });
+
+
+    // shop slider
+
+    const shopSlider = $('#shopSlider');
+
+    shopSlider.owlCarousel({
+        item: 3,
+        dots: false,
+        smartSpeed: 500,
+        margin: 2
+    });
+
+      // Go to the next item
+    $('#shopSliderRight').click(function() {
+        shopSlider.trigger('next.owl.carousel', [500]);
+    });
+
+    // Go to the previous item
+    $('#shopSliderLeft').click(function() {
+        shopSlider.trigger('prev.owl.carousel', [500]);
+    });
+
+});
