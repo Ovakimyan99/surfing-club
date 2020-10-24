13e536865126aadb9521520bcdd4d622257e300d
Тигран <tigr.kman@gmail.com>
2020-10-24 09:32:09 +0300

    завершил делать адаптив

--- js/main.js
+++ js/main.js
@@ -44,10 +44,19 @@ $(document).ready(function() {
     const shopSlider = $('#shopSlider');
 
     shopSlider.owlCarousel({
-        item: 3,
+        items: 3,
         dots: false,
         smartSpeed: 500,
-        margin: 2
+        margin: 2,
+        responsive: {
+            0 : {
+                items: 1
+            },
+            
+            1254: {
+                items: 3
+            },
+        }
     });
 
       // Go to the next item
