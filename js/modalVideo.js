bc8f801b52a1e937a868b61b9d8f2b24ff1f4ad7
Тигран <tigr.kman@gmail.com>
2020-10-30 21:00:30 +0300

    Иначе реализовал вывод видео в модалке

--- js/modalVideo.js
+++ js/modalVideo.js
@@ -3,11 +3,20 @@
 const modalVideoWrapper = document.querySelector('#modal-video-wrapper');
 const viedoControl = document.querySelector('.club-description-video__control');
 const videoModalClose = document.querySelectorAll('[data-video-modal-close]');
+const modalVideo = document.querySelector('.modal-block-video');
 const body = document.body;
 
 viedoControl.addEventListener('click', ()=>{
     modalVideoWrapper.style.display = 'flex';
     body.style.overflow = 'hidden';
+    
+    const modalVideoIframe = `<iframe width="560" height="315" src="https://www.youtube.com/embed/cYjy5BNQWbA" frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
+    picture-in-picture" allowfullscreen></iframe>`;
+
+    if(!modalVideo.querySelector('iframe')){
+        modalVideo.insertAdjacentHTML('afterbegin', modalVideoIframe);   
+    }
 });
 
 
